2d32e353577494a42edac0f5e64a156f636f154a
José Lara <josselara1@gmail.com>
2023-06-11 16:59:06 -0300

    cambios en los cards de los campeones

--- src/components/champs/CardChamp.tsx
+++ src/components/champs/CardChamp.tsx
@@ -7,7 +7,7 @@ import FavButton from '../../components/buttons/FavButton'
 import { useEffect, useRef, useState } from 'react';
 
 /**
- * Component that display the card with the information of the champion
+ * Component that display the card with the information of the champion 
  */
 function CardChamp({ id, image, title, tags }: IChamps): JSX.Element {
 
@@ -34,29 +34,52 @@ function CardChamp({ id, image, title, tags }: IChamps): JSX.Element {
 
 
     return (
+        // <li key={id} className={`card_champ ${isIntersecting ? 'show' : 'hidden'}`} ref={elementoRef}>
+        //     <div className='card_champ_img'>
+        //         <Link to={`/champions/${id}`} className='card_champ_link' id={id}>
+        //             <img src={image} alt={image} />
+        //         </Link>
+        //         <FavButton id={id} />
+        //         <div className='card_champ_info'>
+        //             <div className='card_champ_info_name'>{splitName(id)}</div>
+        //             <div className='card_champ_info_title'> {title}</div>
+        //             <div className='card_champ_info_tags'>
+        //                 {tags.map(tag => {
+        //                     return (
+        //                         <Tooltip title={`${tag}`} arrow key={`${tag}`} >
+        //                             <img
+        //                                 src={`/tags/${tag}.png`}
+        //                                 alt={`${id + tag}`}
+        //                                 key={`${id + tag}`}
+        //                             />
+        //                         </Tooltip>
+        //                     )
+        //                 })}
+        //             </div>
+        //         </div>
+        //     </div>
+        // </li>
+        // ------------------------------------------------------------------------
         <li key={id} className={`card_champ ${isIntersecting ? 'show' : 'hidden'}`} ref={elementoRef}>
-            <div className='card_champ_img'>
-                <Link to={`/champions/${id}`} className='card_champ_link' id={id}>
-                    <img src={image} alt={image} />
-                </Link>
-                <FavButton id={id} />
-                <div className='card_champ_info'>
-                    <div className='card_champ_info_name'>{splitName(id)}</div>
-                    <div className='card_champ_info_title'> {title}</div>
-                    <div className='card_champ_info_tags'>
-                        {tags.map(tag => {
-                            return (
-                                <Tooltip title={`${tag}`} arrow key={`${tag}`} >
-                                    <img
-                                        src={`/tags/${tag}.png`}
-                                        alt={`${id + tag}`}
-                                        key={`${id + tag}`}
-                                    />
-                                </Tooltip>
-                            )
-                        })}
-                    </div>
+            <div className="card_champ_content">
+                <span className='card_champ_content_title'>{title}</span>
+            </div>
+            <img src={image} className='card_champ_img' alt={image} />
+            <div className="card_champ_info">
+                <div className='card_champ_info_tags'>
+                    {tags.map(tag => {
+                        return (
+                            <Tooltip title={`${tag}`} arrow key={`${tag}`} >
+                                <img
+                                    src={`/tags/${tag}.png`}
+                                    alt={`${id + tag}`}
+                                    key={`${id + tag}`}
+                                />
+                            </Tooltip>
+                        )
+                    })}
                 </div>
+                <span className="card_champ_info_name">{splitName(id)}</span>
             </div>
         </li>
     )
